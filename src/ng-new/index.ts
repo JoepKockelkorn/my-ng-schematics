181ce312be82d48300d110a0e291338ec220ad57
Joep Kockelkorn <joep.kockelkorn@qnh.nl>
2019-02-08 16:22:44 +0100

    refactor: test succeeded

--- src/ng-new/index.ts
+++ src/ng-new/index.ts
@@ -2,7 +2,7 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 import { Schema } from './schema';
 
-export function ngNew(_options: Schema): Rule {
+export default function(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     tree.create('hello', 'world');
     return tree;
--- src/ng-new/index_spec.ts
+++ src/ng-new/index_spec.ts
@@ -2,13 +2,19 @@ import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 
+import { Schema } from './schema';
+
 const collectionPath = path.join(__dirname, '../collection.json');
 
 describe('ng-new', () => {
   it('works', () => {
     const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = runner.runSchematic('ng-new', {}, Tree.empty());
+    const options: Schema = {
+      name: 'test',
+      version: '7'
+    };
+    const tree = runner.runSchematic('ng-new', options, Tree.empty());
 
-    expect(tree.files).toEqual([]);
+    expect(tree.files.length).toEqual(1);
   });
 });
