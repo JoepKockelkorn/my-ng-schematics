4117aba742093d1b34053c1473baca431195da0b
Joep Kockelkorn <joep.kockelkorn@qnh.nl>
2019-02-08 09:32:28 +0100

    chore: initial commit

---
+++ src/ng-new/index.ts
@@ -0,0 +1,8 @@
+import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
+
+export function ngNew(_options: any): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    tree.create("hello", "world");
+    return tree;
+  };
+}
